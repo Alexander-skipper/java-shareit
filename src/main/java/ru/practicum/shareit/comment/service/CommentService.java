ba378ab885ca5930bf69eb197e26c42733753879
Alexander <alexanderskipper@yandex.ru>
2025-12-14 13:27:26 +0400

    feat: fz16

--- src/main/java/ru/practicum/shareit/comment/service/CommentService.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.comment.service;
-
-import ru.practicum.shareit.comment.dto.CommentDto;
-import ru.practicum.shareit.comment.dto.CommentRequestDto;
-
-public interface CommentService {
-    CommentDto createComment(CommentRequestDto commentRequestDto, Long itemId, Long authorId);
-}
